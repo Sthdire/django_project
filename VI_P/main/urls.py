0d118a9402fe9bfe6927a7e7035c746e0f23b3c1
Settter <egor-udon@mail.ru>
2022-08-05 02:56:02 +0300

    changes

--- VI_P/main/urls.py
+++ VI_P/main/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.utils.termcolors import background
+
 from . import views
 
 urlpatterns = [
-
-    path('', views.render_s)
+    path('', views.main_render),
+    path('profile', views.profile_render),
+    path('background', views.background_render),
 ]
--- VI_P/main/views.py
+++ VI_P/main/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 
-def render_s(request):
+def main_render(request):
     return render(request, 'temp/index.html')
+
+def profile_render(request):
+    return render(request, 'temp/profile.html')
+
+def background_render(request):
+    return render(request, 'temp/background.html')
