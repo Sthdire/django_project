168194030b535d87ec81dbb1335cb60db07a9fae
Settter <egor-udon@mail.ru>
2022-08-05 03:18:45 +0300

    changes 2

--- VI_P/main/urls.py
+++ VI_P/main/urls.py
@@ -1,7 +1,4 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.utils.termcolors import background
-
+from django.urls import path
 from . import views
 
 urlpatterns = [
